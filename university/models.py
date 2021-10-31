3bd224922cb82faf13cbf41e4322b7054fc6c66d
bogdan <bbv1052@gmail.com>
2021-10-31 20:20:56 +0200

    create model

---
+++ university/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class University(models.Model):
+    name = models.CharField(max_length=64)
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=64)
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=64)
+
+class Group(models.Model):
+    name = models.CharField(max_length=64)
+
+class Student(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    group = models.CharField(max_length=64)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    faculty = models.CharField(max_length=64)
+
+
+
+
+
+
+
+
+
+
+
+
